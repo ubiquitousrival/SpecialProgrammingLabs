44e74da3819b1fe4e55d8247214eebbe41044e5c
ubiquitousrival <ubiquitous.rival@gmail.com>
2025-12-14 17:45:51 +0200

    Moved deploy.prototxt and res(...) installation to scripts/preinstall.sh and updated .gitignore, scripts/build.sh

--- Lab4/scripts/build.sh
+++ Lab4/scripts/build.sh
@@ -7,3 +7,9 @@ cd build || exit
 
 cmake ..
 make
+
+echo "Copying models to build directory..."
+cp ../models/deploy.prototxt .
+cp ../models/res10_300x300_ssd_iter_140000.caffemodel .
+
+echo "Build complete."
