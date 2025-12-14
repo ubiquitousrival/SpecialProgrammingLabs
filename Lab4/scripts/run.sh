61df4636fc63df807a0158030ed26b0843c9ce5d
ubiquitousrival <ubiquitous.rival@gmail.com>
2025-12-14 16:19:27 +0000

    Updated scripts/run.sh and CMakeLists.txt

--- Lab4/scripts/run.sh
+++ Lab4/scripts/run.sh
@@ -2,15 +2,16 @@
 
 cd "$(dirname "$0")/.." || exit
 
-APP_NAME="lab4_app"
+APP_NAME="lab4_app" 
 
 if [ -f "./build/$APP_NAME" ]; then
     echo "Starting $APP_NAME..."
-    ./build/$APP_NAME
+
+    cd build || exit
+
+    ./$APP_NAME
 else
-    echo "Error: Executable './build/$APP_NAME' not found."
-    echo "Files in build folder:"
-    ls -F ./build/ 2>/dev/null
+    echo "Error: Executable not found inside build directory."
     echo "Please run './scripts/build.sh' first."
     exit 1
 fi
