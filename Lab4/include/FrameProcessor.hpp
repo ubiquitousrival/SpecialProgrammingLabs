dc504e7187f0abe0dde1069ca685b93e7b46ea66
ubiquitousrival <ubiquitous.rival@gmail.com>
2025-12-18 22:59:32 +0200

    Fix

--- Lab4/include/FrameProcessor.hpp
+++ Lab4/include/FrameProcessor.hpp
@@ -1,13 +1,27 @@
 #pragma once
-#include <opencv2/core.hpp>
-#include <opencv2/imgproc.hpp>
-#include "KeyProcessor.hpp"
-#include "FaceDetector.hpp"
+#include <opencv2/opencv.hpp>
+
+enum class ProcessMode {
+    None = 0,
+    Invert,
+    Blur,
+    Canny,
+    Sobel,
+    Mirror,
+    Sepia,
+    Pixelize
+};
 
 class FrameProcessor {
-private:
-    FaceDetector detector;
-
 public:
-    void process(cv::Mat& frame, ProcessMode mode, int brightnessVal, int effectVal, int lagVal, bool isDetectionEnabled);
+    void process(cv::Mat& frame, ProcessMode mode, int brightness, int effectValue, int lagValue, bool faceDetection);
+
+private:
+    void applyInvert(cv::Mat& frame);
+    void applyBlur(cv::Mat& frame, int value);
+    void applyCanny(cv::Mat& frame, int value);
+    void applySobel(cv::Mat& frame);
+    void applyMirror(cv::Mat& frame);
+    void applySepia(cv::Mat& frame);
+    void applyPixelize(cv::Mat& frame, int value);
 };
