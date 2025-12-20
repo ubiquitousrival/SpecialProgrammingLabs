2c2e9d0e0379ed0205745386c44bc483f4f6f441
ubiquitousrival <ubiquitous.rival@gmail.com>
2025-12-20 19:22:47 +0200

    Restored missing model configuration methods in ConfigManager

--- Lab4/include/ConfigManager.hpp
+++ Lab4/include/ConfigManager.hpp
@@ -8,6 +8,10 @@ private:
     int frameWidth;
     int frameHeight;
     std::string logLevel;
+
+    std::string modelProto;
+    std::string modelWeights;
+    float modelThreshold;
     
     ConfigManager();
     static ConfigManager* instance;
@@ -27,8 +31,16 @@ public:
     int getFrameHeight(int defaultVal = 480) const;
     std::string getLogLevel(const std::string& defaultVal = "INFO") const;
 
+    std::string getModelProto(const std::string& defaultVal = "deploy.prototxt") const;
+    std::string getModelWeights(const std::string& defaultVal = "res10_300x300_ssd_iter_140000.caffemodel") const;
+    float getModelThreshold(float defaultVal = 0.5f) const;
+
     void setCameraId(int id);
     void setFrameWidth(int width);
     void setFrameHeight(int height);
     void setLogLevel(const std::string& level);
+
+    void setModelProto(const std::string& proto);
+    void setModelWeights(const std::string& weights);
+    void setModelThreshold(float threshold);
 };
--- Lab4/src/ConfigManager.cpp
+++ Lab4/src/ConfigManager.cpp
@@ -6,7 +6,12 @@
 ConfigManager* ConfigManager::instance = nullptr;
 std::mutex ConfigManager::mutex_;
 
-ConfigManager::ConfigManager() : cameraId(0), frameWidth(640), frameHeight(480), logLevel("INFO") {}
+ConfigManager::ConfigManager() 
+    : cameraId(0), frameWidth(640), frameHeight(480), logLevel("INFO"),
+      modelProto("deploy.prototxt"), 
+      modelWeights("res10_300x300_ssd_iter_140000.caffemodel"), 
+      modelThreshold(0.5f) 
+{}
 
 ConfigManager& ConfigManager::getInstance() {
     std::lock_guard<std::mutex> lock(mutex_);
@@ -38,6 +43,23 @@ void ConfigManager::loadConfig(const std::string& filename) {
                 logLevel = line.substr(start + 3, end - start - 3);
             }
         }
+        else if (line.find("\"proto\"") != std::string::npos) {
+            size_t start = line.find(": \"");
+            size_t end = line.find("\"", start + 3);
+            if (start != std::string::npos && end != std::string::npos) {
+                modelProto = line.substr(start + 3, end - start - 3);
+            }
+        }
+        else if (line.find("\"weights\"") != std::string::npos) {
+            size_t start = line.find(": \"");
+            size_t end = line.find("\"", start + 3);
+            if (start != std::string::npos && end != std::string::npos) {
+                modelWeights = line.substr(start + 3, end - start - 3);
+            }
+        }
+        else if (line.find("\"threshold\"") != std::string::npos) {
+             try { modelThreshold = std::stof(line.substr(line.find(":") + 1)); } catch(...) {}
+        }
     }
     file.close();
     Logger::getInstance().info("Configuration loaded from " + filename);
@@ -58,6 +80,11 @@ void ConfigManager::saveConfig(const std::string& filename) {
     file << "    },\n";
     file << "    \"logging\": {\n";
     file << "        \"level\": \"" << logLevel << "\"\n";
+    file << "    },\n";
+    file << "    \"model\": {\n";
+    file << "        \"proto\": \"" << modelProto << "\",\n";
+    file << "        \"weights\": \"" << modelWeights << "\",\n";
+    file << "        \"threshold\": " << modelThreshold << "\n";
     file << "    }\n";
     file << "}";
     
@@ -70,7 +97,15 @@ int ConfigManager::getFrameWidth(int defaultVal) const { return frameWidth; }
 int ConfigManager::getFrameHeight(int defaultVal) const { return frameHeight; }
 std::string ConfigManager::getLogLevel(const std::string& defaultVal) const { return logLevel; }
 
+std::string ConfigManager::getModelProto(const std::string& defaultVal) const { return modelProto; }
+std::string ConfigManager::getModelWeights(const std::string& defaultVal) const { return modelWeights; }
+float ConfigManager::getModelThreshold(float defaultVal) const { return modelThreshold; }
+
 void ConfigManager::setCameraId(int id) { cameraId = id; }
 void ConfigManager::setFrameWidth(int width) { frameWidth = width; }
 void ConfigManager::setFrameHeight(int height) { frameHeight = height; }
 void ConfigManager::setLogLevel(const std::string& level) { logLevel = level; }
+
+void ConfigManager::setModelProto(const std::string& proto) { modelProto = proto; }
+void ConfigManager::setModelWeights(const std::string& weights) { modelWeights = weights; }
+void ConfigManager::setModelThreshold(float threshold) { modelThreshold = threshold; }
