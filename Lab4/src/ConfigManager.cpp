82bdfb1b0426a04811676e732f0499ca939a92ae
ubiquitousrival <ubiquitous.rival@gmail.com>
2025-12-20 20:02:59 +0200

    Switched to nlohmann/json library for config management

--- Lab4/src/ConfigManager.cpp
+++ Lab4/src/ConfigManager.cpp
@@ -2,6 +2,10 @@
 #include "Logger.hpp"
 #include <fstream>
 #include <iostream>
+#include <iomanip> 
+
+#include "json.hpp"
+using json = nlohmann::json;
 
 ConfigManager* ConfigManager::instance = nullptr;
 std::mutex ConfigManager::mutex_;
@@ -27,42 +31,32 @@ void ConfigManager::loadConfig(const std::string& filename) {
         Logger::getInstance().error("Could not open config file: " + filename);
         return;
     }
-    
-    std::string line;
-    while (std::getline(file, line)) {
-        if (line.find("\"id\"") != std::string::npos) {
-            try { cameraId = std::stoi(line.substr(line.find(":") + 1)); } catch(...) {}
+
+    try {
+        json j;
+        file >> j;
+
+        if (j.contains("camera")) {
+            cameraId = j["camera"].value("id", cameraId);
+            frameWidth = j["camera"].value("width", frameWidth);
+            frameHeight = j["camera"].value("height", frameHeight);
         }
-        else if (line.find("\"width\"") != std::string::npos) {
-            try { frameWidth = std::stoi(line.substr(line.find(":") + 1)); } catch(...) {}
+
+        if (j.contains("logging")) {
+            logLevel = j["logging"].value("level", logLevel);
         }
-        else if (line.find("\"level\"") != std::string::npos) {
-            size_t start = line.find(": \"");
-            size_t end = line.find("\"", start + 3);
-            if (start != std::string::npos && end != std::string::npos) {
-                logLevel = line.substr(start + 3, end - start - 3);
-            }
+
+        if (j.contains("model")) {
+            modelProto = j["model"].value("proto", modelProto);
+            modelWeights = j["model"].value("weights", modelWeights);
+            modelThreshold = j["model"].value("threshold", modelThreshold);
         }
-        else if (line.find("\"proto\"") != std::string::npos) {
-            size_t start = line.find(": \"");
-            size_t end = line.find("\"", start + 3);
-            if (start != std::string::npos && end != std::string::npos) {
-                modelProto = line.substr(start + 3, end - start - 3);
-            }
-        }
-        else if (line.find("\"weights\"") != std::string::npos) {
-            size_t start = line.find(": \"");
-            size_t end = line.find("\"", start + 3);
-            if (start != std::string::npos && end != std::string::npos) {
-                modelWeights = line.substr(start + 3, end - start - 3);
-            }
-        }
-        else if (line.find("\"threshold\"") != std::string::npos) {
-             try { modelThreshold = std::stof(line.substr(line.find(":") + 1)); } catch(...) {}
-        }
+
+        Logger::getInstance().info("Configuration loaded from " + filename);
+
+    } catch (const json::parse_error& e) {
+        Logger::getInstance().error("JSON parse error: " + std::string(e.what()));
     }
-    file.close();
-    Logger::getInstance().info("Configuration loaded from " + filename);
 }
 
 void ConfigManager::saveConfig(const std::string& filename) {
@@ -72,21 +66,19 @@ void ConfigManager::saveConfig(const std::string& filename) {
         return;
     }
 
-    file << "{\n";
-    file << "    \"camera\": {\n";
-    file << "        \"id\": " << cameraId << ",\n";
-    file << "        \"width\": " << frameWidth << ",\n";
-    file << "        \"height\": " << frameHeight << "\n";
-    file << "    },\n";
-    file << "    \"logging\": {\n";
-    file << "        \"level\": \"" << logLevel << "\"\n";
-    file << "    },\n";
-    file << "    \"model\": {\n";
-    file << "        \"proto\": \"" << modelProto << "\",\n";
-    file << "        \"weights\": \"" << modelWeights << "\",\n";
-    file << "        \"threshold\": " << modelThreshold << "\n";
-    file << "    }\n";
-    file << "}";
+    json j;
+
+    j["camera"]["id"] = cameraId;
+    j["camera"]["width"] = frameWidth;
+    j["camera"]["height"] = frameHeight;
+    
+    j["logging"]["level"] = logLevel;
+    
+    j["model"]["proto"] = modelProto;
+    j["model"]["weights"] = modelWeights;
+    j["model"]["threshold"] = modelThreshold;
+
+    file << j.dump(4); 
     
     file.close();
     Logger::getInstance().info("Configuration saved to " + filename);
@@ -96,7 +88,6 @@ int ConfigManager::getCameraId(int defaultVal) const { return cameraId; }
 int ConfigManager::getFrameWidth(int defaultVal) const { return frameWidth; }
 int ConfigManager::getFrameHeight(int defaultVal) const { return frameHeight; }
 std::string ConfigManager::getLogLevel(const std::string& defaultVal) const { return logLevel; }
-
 std::string ConfigManager::getModelProto(const std::string& defaultVal) const { return modelProto; }
 std::string ConfigManager::getModelWeights(const std::string& defaultVal) const { return modelWeights; }
 float ConfigManager::getModelThreshold(float defaultVal) const { return modelThreshold; }
@@ -105,7 +96,6 @@ void ConfigManager::setCameraId(int id) { cameraId = id; }
 void ConfigManager::setFrameWidth(int width) { frameWidth = width; }
 void ConfigManager::setFrameHeight(int height) { frameHeight = height; }
 void ConfigManager::setLogLevel(const std::string& level) { logLevel = level; }
-
 void ConfigManager::setModelProto(const std::string& proto) { modelProto = proto; }
 void ConfigManager::setModelWeights(const std::string& weights) { modelWeights = weights; }
 void ConfigManager::setModelThreshold(float threshold) { modelThreshold = threshold; }
